41ddd978cc37f837b05e9925f17d782f4c41af90
Anish Chhetri <anishchhetri196@gmail.com>
2022-12-23 20:13:51 +0545

    remove dispatcher

--- src/lib/StartQuizButton.svelte
+++ src/lib/StartQuizButton.svelte
@@ -1,18 +1,7 @@
 <script lang="ts">
-  import { createEventDispatcher } from "svelte";
-
   export let className = "";
-
-  const dispatcher = createEventDispatcher();
-
-  function handleClick() {
-    dispatcher("startQuiz");
-  }
 </script>
 
-<button
-  on:click={handleClick}
-  class="start-quiz-btn py-3 px-7 font-bold text-lg {className}"
->
+<button on:click class="start-quiz-btn py-3 px-7 font-bold text-lg {className}">
   Start Quiz</button
 >
