cae0055fad86bc5dad1e899c5a32a0be023d8060
Anish Chhetri <anishchhetri196@gmail.com>
2022-12-23 20:14:56 +0545

    add onclick

--- src/App.svelte
+++ src/App.svelte
@@ -11,7 +11,7 @@
   <div class="h-screen grid place-content-center gap-10">
     <h1 class="quizzical text-5xl">Quizzical</h1>
     <div class="grid place-content-center">
-      <StartQuizButton on:startQuiz={() => (isQuizStarted = true)} />
+      <StartQuizButton on:click={() => (isQuizStarted = true)} />
     </div>
   </div>
 {/if}
